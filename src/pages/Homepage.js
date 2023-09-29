a0e838fb34dfe2a5add04d6d862740f6860fcd18
Wren Macayan <91804029+hnddrx@users.noreply.github.com>
2023-09-29 15:38:08 +0800

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,36 +6,11 @@ import Map from  '../components/Map';
 //import LeafletMap from '../components/Leaflet';
 
 
-import Particles from "react-particles";
-
-
 const Homepage = () => {
     
     return (
         <div data-aos="fade-up">
-             <Particles
-                style={{ position: "absolute" }}
-                height="95%"
-                width="95%"
-                params={{
-                particles: {
-                    color: {
-                    value: "#000000"
-                    },
-                    line_linked: {
-                    color: {
-                        value: "#000000"
-                    }
-                    },
-                    number: {
-                    value: 50
-                    },
-                    size: {
-                    value: 3
-                    }
-                }
-                }}
-            />
+             
             <Navbar />
             <LandingPage />
             <Skills />
